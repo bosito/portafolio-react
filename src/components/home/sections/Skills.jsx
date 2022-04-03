d263bb482c4e007d7324599584e187eec1ac75a4
DESKTOP-KHKBE2E\bosito <bosito321@gmail.com>
2022-04-03 17:33:44 -0500

    se termino la parte de skills falta la parte de about me donde pondre las experiemcias que he tenido.

--- src/components/home/sections/Skills.jsx
+++ src/components/home/sections/Skills.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Element } from 'react-scroll';
+import list_name_skils from '../../../config/list_name_skils'
 
 //styles
 import '../../../styles/skills.css';
@@ -7,15 +8,44 @@ import '../../../styles/skills.css';
 
 export default function Skills() {
     const [listSkills, setListSkills] = useState([]);
+    const [selectSection, setSelectSection] = useState(true);
+
+    useEffect(() => {
+
+        if (list_name_skils) {
+            setListSkills(list_name_skils);
+        };
+
+    }, []);
 
     return (
         <Element className="skills" id='3'>
             <div className='card_data_skill'>
-
-                <div className='card_skill' >
-                    <p>JavaScript</p>
+                <div className='header_skill' >
+                    <button className='header_button' onClick={() => setSelectSection(true)}>
+                        skills
+                    </button>
+                    <button className='header_button' onClick={() => setSelectSection(false)}>
+                        about me
+                    </button>
                 </div>
-                
+                {
+                    selectSection ?
+                        <div className='list_skils' >
+                            {
+                                listSkills.map((skil) => {
+                                    return (
+                                        <div className='card_skill' >
+                                            <p>{skil.title}</p>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        :
+                        null
+                }
+
             </div>
         </Element>
     );
---
+++ src/config/list_name_skils.js
@@ -0,0 +1,40 @@
+const skils = [
+    {
+        title: 'JavaScript',
+    },
+    {
+        title: 'React.js',
+    },
+    {
+        title: 'React Navite',
+    },
+    {
+        title: 'React Native Expo',
+    },
+    {
+        title: 'React Native Cli',
+    },
+    {
+        title: 'Node.js',
+    },
+    {
+        title: 'Express.js',
+    },
+    {
+        title: 'Html5',
+    },
+    {
+        title: 'Css',
+    },
+    {
+        title: 'Sass',
+    },
+    {
+        title: 'Postgres',
+    },
+    {
+        title: 'Phyton',
+    }
+];
+
+export default skils;
